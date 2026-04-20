5d864c161f8a5bc2d9899fd767eba98c37d7d58d
Akvk33 <akvenky33@gmail.com>
2026-04-20 08:41:28 +0530

    render requirements

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ CORS(
 
 app.config.update(
     SESSION_COOKIE_SAMESITE="None",  
-    SESSION_COOKIE_SECURE=True, 
+    SESSION_COOKIE_SECURE=True,     
      SESSION_COOKIE_DOMAIN="https://library-management-jy5z.onrender.com/"
 )
 
